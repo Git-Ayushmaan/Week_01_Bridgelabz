2158a3d039077020e46234b087351861792ad1ae
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-11 13:37:32 +0530

    added

---
+++ Level-01/KilometersToMiles.java
@@ -0,0 +1,16 @@
+public class KilometersToMiles {
+    public static void main(String[] args) {
+		
+		//declaring kilometers as variable and assigning value 
+        double kilometers = 10.8;
+		
+		//declaring miles as variable 
+        double miles ;
+		
+		//converting kilometers into miles
+        miles = kilometers * 1.6;
+		
+		//output statement
+        System.out.println("The distance 10.8km in miles is:"+ miles);
+    }
+}
