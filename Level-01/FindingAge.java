51324701b1bfeb4809091a6cf2165fa03fb30f6e
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-11 13:36:14 +0530

    added

---
+++ Level-01/FindingAge.java
@@ -0,0 +1,14 @@
+public class FindingAge {
+    public static void main(String[] args) {
+		//Declaring age,currentYear,birthYear as input and assigning values 
+        int age;
+        int currentYear = 2024;
+        int birthYear = 2000;
+		
+		//calculating age 
+        age =  currentYear - birthYear;
+		
+		//output statement
+        System.out.println("Harry's age in 2024 is :" + age);
+    }
+}
