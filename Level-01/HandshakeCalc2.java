151798f9a84da0f52587453025da76221bf4e458
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-13 10:34:09 +0530

    added

---
+++ Level-01/HandshakeCalc2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HandshakeCalc2 {
+
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int n = kb.nextInt();
+
+        if (n < 2) {
+            System.out.println("Less than 2 students .");
+        } else {
+            int handshakes = calculateHandshakes(n);
+            System.out.println("The maximum number of handshakes among " + n + " students is: " + handshakes);
+        }
+
+        kb.close
+		}
+	}
