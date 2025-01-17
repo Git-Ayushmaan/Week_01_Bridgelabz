aef7b17dc016823367237763819433e0114a69a4
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-17 11:10:24 +0530

    Added"

---
+++ Level-04-extras/CompareTwoStrings.java
@@ -0,0 +1,33 @@
+public class CompareTwoStrings {
+
+    public static void main(String[] args) {
+        String str1 = "apple";
+        String str2 = "banana";
+
+        int result = compareStringsLexicographically(str1, str2);
+        
+        if (result < 0) {
+            System.out.println("\"" + str1 + "\" comes before \"" + str2 + "\" in lexicographical order.");
+        } else if (result > 0) {
+            System.out.println("\"" + str1 + "\" comes after \"" + str2 + "\" in lexicographical order.");
+        } else {
+            System.out.println("\"" + str1 + "\" is equal to \"" + str2 + "\" in lexicographical order.");
+        }
+    }
+    
+    public static int compareStringsLexicographically(String str1, String str2) {
+        int length1 = str1.length();
+        int length2 = str2.length();
+        int minLength = Math.min(length1, length2);
+
+        for (int i = 0; i < minLength; i++) {
+            char ch1 = str1.charAt(i);
+            char ch2 = str2.charAt(i);
+            if (ch1 != ch2) {
+                return ch1 - ch2;
+            }
+        }
+
+        return length1 - length2;
+    }
+}
