aaf31af51ae60c7e140beb7e33a03d77f25f8dd5
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-17 11:20:19 +0530

    Added"

---
+++ Level-06-extras/PrimeNoCheck.java
@@ -0,0 +1,15 @@
+public class PrimeNoCheck {
+
+    public static void main(String[] args) {
+        int number = 29; // Example number
+        System.out.println(number + " is prime: " + isPrime(number));
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
