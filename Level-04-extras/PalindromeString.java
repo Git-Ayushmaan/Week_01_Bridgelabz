edca8561e643421833b2a5d9d399c25b946df50b
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-17 11:11:43 +0530

    Added"

---
+++ Level-04-extras/PalindromeString.java
@@ -0,0 +1,23 @@
+public class PalindromeString {
+
+    public static void main(String[] args) {
+        String input = "racecar"; // Replace with input as needed
+        if (checkPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+
+    public static boolean checkPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
